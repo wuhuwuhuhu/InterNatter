f6112dea0a56c09e69e2818ff3e7d4e3f56fd93c
Ameya221 <57459074+Ameya221@users.noreply.github.com>
2021-04-24 16:08:29 -0400

    added notification when user readds friend

--- public/chatrooms/chatrooms.js
+++ public/chatrooms/chatrooms.js
@@ -174,6 +174,7 @@ $(() => {
            // const $target = $(event.target)
             $.post("/users/addFriend", data, function (res) {
                 if (res.status === 1) {
+                    $("p[warn="+senderid+"]").show()
                     let $error = $(`
                     <div class="alert alert-warning" role="alert" style="width:80%; margin-left: 10%;
                     margin-top: 5px;">${getUtilsMap(res.msg)}</div>
